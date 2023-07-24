a3dcbed74bd739a4647f8f6cb815909051d5b7d8
rayhanrayhan <mdrayhanuddinremon02@gmail.com>
2023-07-24 20:48:10 +0600

    fixed some issues

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         const collegeReview = client.db("admissionCamp").collection("reviews");
         const collegeResearchPaper = client.db("admissionCamp").collection("researchPaper");
         const myCollegeCollection = client.db("admissionCamp").collection("mycollege");
